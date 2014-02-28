1c1fe870550db239dfac7271859a866bd8a10507
michaelwang <michaelwang2089@163.com>
2014-02-28 18:10:41 +0800

    add

--- cmake_sy_server.sh
+++ cmake_sy_server.sh
@@ -10,26 +10,26 @@ result=$(echo $1 | grep 'l')
 if [[ $result != "" ]] 
 then
   echo -e '\e[0;33;1mmake logic\e[0m'
-  cd $pro_root/logic && make $param -j4
+  cd $pro_root/logic && make clean && make $param -j4
 fi
 
 result=$(echo $1 | grep 'w')
 if [[ $result != "" ]] 
 then
   echo -e '\e[0;33;1mmake world\e[0m'
-  cd $pro_root/world && make $param -j4
+  cd $pro_root/world && make clean && make $param -j4
 fi
 
 result=$(echo $1 | grep 'd')
 if [[ $result != "" ]] 
 then
   echo -e '\e[0;33;1mmake db_proxy\e[0m'
-  cd $pro_root/db_proxy && make $param -j4
+  cd $pro_root/db_proxy && make clean && make $param -j4
 fi
 
 result=$(echo $1 | grep 'c')
 if [[ $result != "" ]] 
 then
   echo -e '\e[0;33;1mmake chat\e[0m'
-  cd $pro_root/chat && make $param -j4
+  cd $pro_root/chat && make clean && make $param -j4
 fi
